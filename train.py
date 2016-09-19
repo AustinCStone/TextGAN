e1159239555027f6eb3fa16963a02f5bcee9a8d7
Austin Stone <austin@vicarious.com>
2016-09-18 19:22:02 -0700

    Added alternating generator / discriminator training

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
@@ -8,7 +7,8 @@ import os
 from six.moves import cPickle
 
 from utils import TextLoader
-from model import Model
+from gen_model import GenModel
+from disc_model import DiscModel
 
 def main():
     parser = argparse.ArgumentParser()
@@ -20,7 +20,9 @@ def main():
                        help='size of RNN hidden state')
     parser.add_argument('--num_layers', type=int, default=2,
                        help='number of layers in the RNN')
-    parser.add_argument('--model', type=str, default='lstm',
+    parser.add_argument('--gen_model', type=str, default='lstm',
+                       help='rnn, gru, or lstm')
+    parser.add_argument('--disc_model', type=str, default='lstm',
                        help='rnn, gru, or lstm')
     parser.add_argument('--batch_size', type=int, default=50,
                        help='minibatch size')
@@ -28,7 +30,7 @@ def main():
                        help='RNN sequence length')
     parser.add_argument('--num_epochs', type=int, default=50,
                        help='number of epochs')
-    parser.add_argument('--save_every', type=int, default=1000,
+    parser.add_argument('--save_every', type=int, default=1,
                        help='save frequency')
     parser.add_argument('--grad_clip', type=float, default=5.,
                        help='clip gradients at this value')
@@ -37,13 +39,17 @@ def main():
     parser.add_argument('--decay_rate', type=float, default=0.97,
                        help='decay rate for rmsprop')                       
     parser.add_argument('--init_from', type=str, default=None,
-                       help="""continue training from saved model at this path. Path must contain files saved by previous training process: 
+                       help="""continue training from saved gen_model at this path. Path must contain files saved by previous training process: 
                             'config.pkl'        : configuration;
                             'words_vocab.pkl'   : vocabulary definitions;
-                            'checkpoint'        : paths to model file(s) (created by tf).
+                            'checkpoint'        : paths to gen_model file(s) (created by tf).
                                                   Note: this file contains absolute paths, be careful when moving files around;
-                            'model.ckpt-*'      : file(s) with model definition (created by tf)
+                            'gen_model.ckpt-*'      : file(s) with gen_model definition (created by tf)
                         """)
+    parser.add_argument('--disc_train_bound', type=float, default=1.0,
+                       help='train the discriminator (only) until its loss reaches this bound')
+    parser.add_argument('--gen_train_bound', type=float, default=-2.0,
+                       help='train the generator (only) until its loss reaches this bound')
     args = parser.parse_args()
     train(args)
 
@@ -51,7 +57,7 @@ def train(args):
     data_loader = TextLoader(args.data_dir, args.batch_size, args.seq_length)
     args.vocab_size = data_loader.vocab_size
     
-    # check compatibility if training is continued from previously saved model
+    # check compatibility if training is continued from previously saved gen_model
     if args.init_from is not None:
         # check if all necessary files exist 
         assert os.path.isdir(args.init_from)," %s must be a a path" % args.init_from
@@ -59,53 +65,84 @@ def train(args):
         assert os.path.isfile(os.path.join(args.init_from,"words_vocab.pkl")),"words_vocab.pkl.pkl file does not exist in path %s" % args.init_from
         ckpt = tf.train.get_checkpoint_state(args.init_from)
         assert ckpt,"No checkpoint found"
-        assert ckpt.model_checkpoint_path,"No model path found in checkpoint"
+        assert ckpt.model_checkpoint_path,"No gen_model path found in checkpoint"
 
         # open old config and check if models are compatible
         with open(os.path.join(args.init_from, 'config.pkl'), 'rb') as f:
             saved_model_args = cPickle.load(f)
-        need_be_same=["model","rnn_size","num_layers","seq_length"]
+        need_be_same=["gen_model","rnn_size","num_layers","seq_length"]
         for checkme in need_be_same:
-            assert vars(saved_model_args)[checkme]==vars(args)[checkme],"Command line argument and saved model disagree on '%s' "%checkme
+            assert vars(saved_model_args)[checkme]==vars(args)[checkme],"Command line argument and saved gen_model disagree on '%s' "%checkme
         
         # open saved vocab/dict and check if vocabs/dicts are compatible
         with open(os.path.join(args.init_from, 'words_vocab.pkl'), 'rb') as f:
             saved_words, saved_vocab = cPickle.load(f)
-        assert saved_words==data_loader.words, "Data and loaded model disagreee on word set!"
-        assert saved_vocab==data_loader.vocab, "Data and loaded model disagreee on dictionary mappings!"
+        assert saved_words==data_loader.words, "Data and loaded gen_model disagreee on word set!"
+        assert saved_vocab==data_loader.vocab, "Data and loaded gen_model disagreee on dictionary mappings!"
         
     with open(os.path.join(args.save_dir, 'config.pkl'), 'wb') as f:
         cPickle.dump(args, f)
     with open(os.path.join(args.save_dir, 'words_vocab.pkl'), 'wb') as f:
         cPickle.dump((data_loader.words, data_loader.vocab), f)
-        
-    model = Model(args)
+    
+    disc_model = DiscModel(args)
+    gen_model = GenModel(args, disc_model)
 
     with tf.Session() as sess:
         tf.initialize_all_variables().run()
         saver = tf.train.Saver(tf.all_variables())
-        # restore model
+        # restore gen_model
         if args.init_from is not None:
             saver.restore(sess, ckpt.model_checkpoint_path)
+        training_disc = True
+        training_gen = False
         for e in range(args.num_epochs):
-            sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
+            sess.run(tf.assign(gen_model.lr, args.learning_rate * (args.decay_rate ** e)))
             data_loader.reset_batch_pointer()
-            state = model.initial_state.eval()
+            #state = gen_model.initial_state.eval()
             for b in range(data_loader.num_batches):
                 start = time.time()
                 x, y = data_loader.next_batch()
-                feed = {model.input_data: x, model.targets: y, model.initial_state: state}
-                train_loss, state, _ = sess.run([model.cost, model.final_state, model.train_op], feed)
+                # TODO: Why must the input be 4 X rnn_size?
+                gen_feed = {gen_model.input_data: x, gen_model.initial_state: np.random.uniform(-1., 1., (args.batch_size, 4*args.rnn_size))}
+                gen_outputs = sess.run([gen_model.outputs], gen_feed)
+                # Convert the input tokens input word vectors
+                with tf.variable_scope('rnnlm', reuse=True):
+                    with tf.device("/cpu:0"):
+                        embedding = tf.get_variable('embedding', [args.vocab_size, args.rnn_size])
+                        real_inputs = tf.nn.embedding_lookup(embedding, x)
+                outputs_wv = np.asarray(gen_outputs[0]).transpose(1, 0, 2)
+                # mix batch with generated data and real data
+                disc_input_feed = np.concatenate((sess.run(real_inputs), outputs_wv), axis=0)
+                print(disc_input_feed.shape)
+                # just a vector of batch_size ones followed by batch_size zeros
+                disc_targets_feed = np.concatenate((np.ones((args.batch_size, 1)), np.zeros((args.batch_size, 1))), axis=0)
+                disc_feed = {disc_model.input_data_wv: disc_input_feed, disc_model.targets: disc_targets_feed}
+                train_outputs = [disc_model.cost_wv, gen_model.cost]
+                if training_gen:
+                    train_outputs = [gen_model.train_op] + train_outputs
+                if training_disc:
+                    train_outputs = [disc_model.train_op_wv] + train_outputs
+                outputs = sess.run(train_outputs, disc_feed)
+                disc_train_loss, gen_train_loss = outputs[-2:]
+                if disc_train_loss < args.disc_train_bound:
+                    training_disc = False
+                    training_gen = True
+                    print 'training the generator only...'
+                if gen_train_loss < args.gen_train_bound:
+                    training_gen = False
+                    training_disc = True
+                    print 'training the discriminator only...'
                 end = time.time()
-                print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}" \
+                print "{}/{} (epoch {}), disc_train_loss = {:.3f}, gen_train_loss = {:.3f}, time/batch = {:.3f}" \
                     .format(e * data_loader.num_batches + b,
                             args.num_epochs * data_loader.num_batches,
-                            e, train_loss, end - start))
+                            e, disc_train_loss, gen_train_loss, end - start)
                 if (e * data_loader.num_batches + b) % args.save_every == 0 \
                         or (e==args.num_epochs-1 and b == data_loader.num_batches-1): # save for the last result
-                    checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+                    checkpoint_path = os.path.join(args.save_dir, 'gen_model.ckpt')
                     saver.save(sess, checkpoint_path, global_step = e * data_loader.num_batches + b)
-                    print("model saved to {}".format(checkpoint_path))
+                    print "gen_model saved to {}".format(checkpoint_path)
 
 if __name__ == '__main__':
     main()
